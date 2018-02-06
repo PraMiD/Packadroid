b25f6d1b6753fe5a0f9d55c5bb15a81518745854
Marko Dorfhuber <markodorfhuber@gmx.de>
2018-02-06 20:44:58 +0100

    Added payload sources

---
+++ payload_src/app/src/main/java/com/tum/team05/genericpayload/GenericPayload.java
@@ -0,0 +1,16 @@
+package com.tum.team05.genericpayload;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by pramid on 24.01.18.
+ */
+
+public class GenericPayload
+{
+    public static void payloadStart(Context c)
+    {
+        Toast.makeText(c, "TestTestTest", 10).show();
+    }
+}
